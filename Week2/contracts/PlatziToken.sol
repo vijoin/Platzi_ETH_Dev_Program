4c70530a9825ba0376f3b173651232d46a9f92b2
Victor Inojosa <vijoin@gmail.com>
2023-03-22 11:02:54 -0300

    Week2: Challenge 2: Prevent from non-owner to mint new tokens

--- Week2/contracts/PlatziToken.sol
+++ Week2/contracts/PlatziToken.sol
@@ -3,12 +3,17 @@
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PlatziToken is ERC777 {
+contract PlatziToken is ERC777, Ownable {
     constructor(
         uint256 _initialSupply,
         address[] memory _defaultOperators
     ) ERC777("PlatziToken", "PLT", _defaultOperators) {
         _mint(msg.sender, _initialSupply, "", "");
+    }
+
+    function mint(address _owner, uint256 _amount) public onlyOwner {
+        _mint(_owner, _amount, "", "");
     }
 }
--- Week2/test/PlatziToken.js
+++ Week2/test/PlatziToken.js
@@ -2,21 +2,38 @@ const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect } = require("chai");
 
 describe("Deploy Token Contract", function () {
-    async function deployFixture() {
-        const Token = await ethers.getContractFactory("PlatziToken");
-        const [ owner ] = await ethers.getSigners();
+  async function deployFixture() {
+    const Token = await ethers.getContractFactory("PlatziToken");
+    const [owner, addr1] = await ethers.getSigners();
 
-        const platziToken = await Token.deploy(1000, []);
+    const platziToken = await Token.deploy(1000, []);
 
-        await platziToken.deployed();
+    await platziToken.deployed();
 
-        return { platziToken, owner };
-    }
+    return { platziToken, owner, addr1 };
+  }
 
-    it("Contract is deployed and all minted tokens are assigned to the deployer", async function () {
-       const { platziToken, owner } = await loadFixture(deployFixture);
-        
-       const ownerBalance = await platziToken.balanceOf(owner.address);
-       expect(await platziToken.totalSupply()).to.equal(ownerBalance);
-    });
-})
+  it("Contract is deployed and all minted tokens are assigned to the deployer", async function () {
+    const { platziToken, owner } = await loadFixture(deployFixture);
+
+    const ownerBalance = await platziToken.balanceOf(owner.address);
+    expect(await platziToken.totalSupply()).to.equal(ownerBalance);
+  });
+
+  it("Only owner can mint new tokens", async function () {
+    const { platziToken, owner } = await loadFixture(deployFixture);
+
+    await platziToken.mint(owner.address, 500);
+
+    const ownerBalance = await platziToken.balanceOf(owner.address);
+    expect(await platziToken.totalSupply()).to.equal(ownerBalance);
+  });
+
+  it("Mint reverted if address is not the owner", async function () {
+    const { platziToken, addr1 } = await loadFixture(deployFixture);
+
+    await expect(
+      platziToken.connect(addr1).mint(addr1.address, 500)
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+});
